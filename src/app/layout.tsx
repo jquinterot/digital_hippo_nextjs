ab732afda134bebad69d6cd499dc74c1a3ee5e81
Johany Quintero <johandquintero@utp.edu.co>
2024-01-03 14:00:05 -0500

    add main components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,11 +13,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-full'>
+      <body className={cn('relative h-full font-sans antialiased', inter.className)}>
+      
+        <main className='relative flex flex-col min-h-screen'>
+          <div className='flex-grow flex-1'>{children}</div>
+        </main>
+      </body>
     </html>
   )
 }
